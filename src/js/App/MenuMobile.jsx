8d078c3f04bb84b4908e021b200d0de8213d0160
Florian Didron <florian@111studio.jp>
2016-09-03 15:46:00 +0900

    Adds Menu content + typography fixes

--- src/js/App/MenuMobile.jsx
+++ src/js/App/MenuMobile.jsx
@@ -6,7 +6,8 @@ const strings = {
   menu2: 'サービス',
   menu3: 'チーム',
   menu4: 'お問い合わせ',
-  menu5: 'メルマガ登録'
+  menu5: 'メルマガ登録',
+  social: 'ソーシャル'
 }
 
 export default class Menu extends React.Component {
@@ -22,12 +23,48 @@ export default class Menu extends React.Component {
     });
   }
 
+  handleScroll = (e, selector) => {
+    e.preventDefault();
+    this.props.scroll(e.target.name);
+    this.setState({opened: false});
+  }
+
   render() {
     const buttonClass = (this.state.opened) ? 'hamburger hamburger--slider is-active' : 'hamburger hamburger--slider';
     const overlayClass = (this.state.opened) ? 'overlay opened' : 'overlay';
     return (
       <Row className="menu-mobile" wide="hide" large="hide" medium="hide" small={ 1 }>
         <div className={ overlayClass }>
+          <h1>
+            <a name="company" href="" onClick={ this.handleScroll.bind(this) }>{ strings.menu1 }</a>
+          </h1>
+          <h1>
+            <a name="service" href="" onClick={ this.handleScroll.bind(this) }>{ strings.menu2 }</a>
+          </h1>
+          <h1>
+            <a name="team" href="" onClick={ this.handleScroll.bind(this) }>{ strings.menu3 }</a>
+          </h1>
+          <h1>
+            <a name="contact" href="" onClick={ this.handleScroll.bind(this) }>{ strings.menu4 }</a>
+          </h1>
+          <h1>
+            <a href="#">{ strings.menu5 }</a>
+          </h1>
+          <h2>{ strings.social }</h2>
+          <Row justify="space-around">
+            <Item>
+              <img src={ require('../../image/instagram.png') } alt="Instagram"/>
+            </Item>
+            <Item>
+              <img src={ require('../../image/facebook.png') } alt="Facebook"/>
+            </Item>
+            <Item>
+              <img src={ require('../../image/twitter.png') } alt="Twitter"/>
+            </Item>
+            <Item>
+              <img src={ require('../../image/linkedin.png') } alt="LinkedIn"/>
+            </Item>
+          </Row>
         </div>
         <Row align="center" className="content" justify="space-between">
           <Item size={ 1 }>
